78be05f8e1f4fe1b36b08438f9718380b12cf2a4
almazuulu <almazuulu@gmail.com>
2022-12-18 16:26:36 +0600

    Tags Comments Descriptions

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ def get_pages(page, page_size):
     return {'message': f'Page size {page_size} and we are on {page}'}
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)
+@app.get('/blog/{id}',
+         summary='get specific blog by id',
+         description='passing id and getting back specific blog when it finds', 
+         status_code=status.HTTP_404_NOT_FOUND, 
+         tags=['blog'])
 def get_blog(id: int, response: Response): # we can define some datatype to accept, it validates automatically
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -32,6 +36,13 @@ def get_blog(id: int, response: Response): # we can define some datatype to acce
         return {"message": f'Blog with id {id}'}
 
 
+@app.get('/blog/{id}/comments/{cid}', 
+         summary='get blog id and comments id',
+         description='getting comments for specific  blog',
+         response_description='Here is your all comments:',
+         tags=['blog', 'comment'])
+def commments_get(blog_id, comment_id):
+    return {'message': f'Blog id: {blog_id}, and comment id: {comment_id}'}
 # predefined path
 class BlogType(str, Enum):
     short = 'short'
@@ -42,7 +53,7 @@ class BlogType(str, Enum):
 @app.get('/blog/type/{type}')
 def get_blog_type(type: BlogType):
     return {'message': f'Blog type {type}'}
-    
+
 # @app.post('/')
 # def hello_world2():
 #     return "Hello World"
